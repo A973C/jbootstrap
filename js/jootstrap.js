3ab3e6b285214cd9c05043aaee4cc36c62a3fa84
anibal <anibal@anibal-desktop>
2012-08-14 16:46:48 -0300

    Update to Bootstrap 2.0.4

--- js/jootstrap.js
+++
@@ -1,7 +0,0 @@
-/* jootstrap.js
-  # author    Prieco S.A.
-  # copyright Copyright (C) 2012 Prieco.com. All Rights Reserved.
-  # @license - http://http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
-  # Websites: http://www.prieco.com
-  # Technical Support:  Forum - http://www.prieco.com/en/forum/index.html
-*/
