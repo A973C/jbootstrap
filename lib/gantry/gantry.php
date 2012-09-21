8685da202f428539f098194a27ec14202d937575
Aníbal Sánchez <anibal.sanchez@prieco.com>
2012-09-21 16:33:33 -0300

    New Gantry4 support, based on pure LESS implementation

--- lib/gantry/gantry.php
+++ lib/gantry/gantry.php
@@ -1,41 +1,39 @@
 <?php
-
 /**
- * @package     Extly.Templates
- * @subpackage  JBootstrap - Twitter's Bootstrap for Joomla (with RocketTheme's Gantry administration)
- * 
- * @author      Prieco S.A. <support@extly.com>
- * @copyright   Copyright (C) 2007 - 2012 Prieco, S.A. All rights reserved.
- * @license     http://http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL 
- * @link        http://www.extly.com http://support.extly.com http://www.prieco.com
+ * @package   gantry
+ * @subpackage bootstrap
+ * @version   ${project.version} ${build_date}
+ * @author    RocketTheme http://www.rockettheme.com
+ * @copyright Copyright (C) 2007 - ${copyright_year} RocketTheme, LLC
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ *
+ * Gantry uses the Joomla Framework (http://www.joomla.org), a GNU/GPLv2 content management system
+ *
  */
-// No direct access
-defined('_JEXEC') or die('Restricted index access');
 
-global $gantry;
+defined('JPATH_BASE') or die();
+/** @var $gantry Gantry */
+		global $gantry;
 
 $gantry_path_j15 = JPATH_SITE . '/components/com_gantry/gantry.php';
 $gantry_path_j16 = JPATH_SITE . '/libraries/gantry/gantry.php';
 
 $gantry_path = '';
-if (version_compare(JVERSION, '1.5', '>=') && version_compare(JVERSION, '1.6', '<'))
-{
-	$gantry_path = $gantry_path_j15;
+if (version_compare(JVERSION, '1.5', '>=') && version_compare(JVERSION, '1.6', '<')) {
+    $gantry_path = $gantry_path_j15;
 }
-elseif (version_compare(JVERSION, '1.6', '>='))
-{
-	$gantry_path = $gantry_path_j16;
+else if (version_compare(JVERSION, '1.6', '>=')) {
+    $gantry_path = $gantry_path_j16;
 }
 
-if (!file_exists($gantry_path))
-{
-	echo JText::_('Unable to find Gantry library.  Please make sure you have it installed.');
-	die;
+if (!file_exists($gantry_path)) {
+    echo JText::_('Unable to find Gantry library.  Please make sure you have it installed.');
+    die;
 }
-require_once $gantry_path;
+require_once($gantry_path);
 
 $app = JFactory::getApplication();
-if (!$app->isAdmin())
-{
-	$app->triggerEvent('onGantryTemplateInit', array($filename));
+if (!$app->isAdmin()){
+    $app->triggerEvent('onGantryInit');
+    $app->triggerEvent('onGantryTemplateInit', array($filename));
 }
